754a3ddfc643cd583bb47ccf96504488236b6bb7
BrianJVarley <brianvarley9@gmail.com>
2019-10-13 21:05:04 +1100

    add webpack config for ng-build

---
+++ webpack.extra.js
@@ -0,0 +1,10 @@
+// modules to exclude during ngx build plus 
+module.exports = {
+    "externals": {
+        "rxjs": "rxjs",
+        "@angular/core": "ng.core",
+        "@angular/common": "ng.common",
+        "@angular/platform-browser": "ng.platformBrowser",
+        "@angular/elements": "ng.elements"
+    }
+}
